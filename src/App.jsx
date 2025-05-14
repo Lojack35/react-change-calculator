df397e9323349fd132c11ff5e3dc6d9c9b1689b9
austinloucks <austin.loucks35@gmail.com>
2025-05-13 17:03:30 -0700

    Mock layout complete. Updated TODO list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 /*
 TODO:
-  - Add a link element to the head of the document to include Bootstrap CSS
-  - Add a section for the user to enter how much money is due, how much money was received, and a button to calculate the change
-  - Add a section to display the change in cards displaying the quantities of 20, 10, 5, 1, quarters, dimes, nickels, and pennies to be returned
+  - Setup state management using useState
+  - Setup event binding for button elements
+  - Update view state based on calculation results
+  - Figure out how to dynamically populate success/fail alert
+  - Possibly create seperate file for building of output cards sections and import it
+
 */
 import "./App.css";
 
@@ -12,19 +15,134 @@ function App() {
   return (
     <div className="container m-4">
       <h1 className="text-left text-primary">Change Calculator</h1>
-      <div className="card w-50 bg-light text-dark shadow rounded">
-        <div className="card-header text-left">Enter Information</div>
-        <div className="card-body">
-          <label htmlFor="amount-due" className="me-2">
-            How much is due?
-          </label>
-          <br />
-          <input
-            className="amount-due text-left"
-            data-testid="amountDue"
-            type="number"
-            placeholder="Enter Amount Due"
-          />
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card bg-light text-dark shadow rounded">
+            <div className="card-header">
+              <h3>Enter Information</h3>
+            </div>
+            <div className="card-body">
+              <label htmlFor="amountDue">How much is due?</label>
+              <input
+                id="amountDue"
+                className="mb-4 form-control"
+                data-testid="amountDue"
+                type="number"
+                placeholder="Enter Amount Due"
+              />
+              <label htmlFor="amountReceived">How much was received?</label>
+              <input
+                id="amountReceived"
+                className="mb-4 form-control"
+                data-testid="amountReceived"
+                type="number"
+                placeholder="Enter Amount Received"
+              />
+            </div>
+            <div className="card-footer text-center">
+              <button
+                className="calculate bg-primary text-light form-control"
+                data-testid="calculate"
+              >
+                Calculate
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <div className="card bg-light text-dark shadow rounded">
+            <div className="card-body">
+              <div
+                id="output"
+                className="card-title alert alert-success text-center"
+                role="alert"
+              >
+                <h3>Success/Fail text displayed here</h3>
+              </div>
+              <div className="row">
+                <div className="col-md-3">
+                  <div className="card bg-light text-dark shadow rounded">
+                    <div className="card-body">
+                      <h4 className="card-title text-center">Twenties</h4>
+                      <p className="card-text text-center">
+                        Amount of twenties to be displayed here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="card bg-light text-dark shadow rounded">
+                    <div className="card-body">
+                      <h4 className="card-title text-center">Tens</h4>
+                      <p className="card-text text-center">
+                        Amount of tens to be displayed here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="card bg-light text-dark shadow rounded">
+                    <div className="card-body">
+                      <h4 className="card-title text-center">Fives</h4>
+                      <p className="card-text text-center">
+                        Amount of fives to be displayed here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="card bg-light text-dark shadow rounded">
+                    <div className="card-body">
+                      <h4 className="card-title text-center">Ones</h4>
+                      <p className="card-text text-center">
+                        Amount of ones to be displayed here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="card bg-light text-dark shadow rounded">
+                    <div className="card-body">
+                      <h4 className="card-title text-center">Quarters</h4>
+                      <p className="card-text text-center">
+                        Amount of quarters to be displayed here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="card bg-light text-dark shadow rounded">
+                    <div className="card-body">
+                      <h4 className="card-title text-center">Dimes</h4>
+                      <p className="card-text text-center">
+                        Amount of dimes to be displayed here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="card bg-light text-dark shadow rounded">
+                    <div className="card-body">
+                      <h4 className="card-title text-center">Nickels</h4>
+                      <p className="card-text text-center">
+                        Amount of nickels to be displayed here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-3">
+                  <div className="card bg-light text-dark shadow rounded">
+                    <div className="card-body">
+                      <h4 className="card-title text-center">Pennies</h4>
+                      <p className="card-text text-center">
+                        Amount of pennies to be displayed here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
